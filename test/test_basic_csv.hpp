04f9a180774ea9f65f539558af894d329cd6a079
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-04-12 21:21:58 -0400

    One more simple unit test with :: delimiter

--- test/test_basic_csv.hpp
+++ test/test_basic_csv.hpp
@@ -33,3 +33,20 @@ TEST_CASE("Parse the most basic of CSV buffers with ', ' delimiter", "[simple cs
     REQUIRE(rows[1]["c"] == "6");
   }
 }
+
+TEST_CASE("Parse the most basic of CSV buffers with '::' delimiter", "[simple csv]") {
+  csv::reader csv;
+  csv.configure()
+    .delimiter("::");
+
+  if (csv.parse("inputs/test_03.csv")) {
+    auto rows = csv.rows();
+    REQUIRE(rows.size() == 2);
+    REQUIRE(rows[0]["a"] == "1");
+    REQUIRE(rows[0]["b"] == "2");
+    REQUIRE(rows[0]["c"] == "3");
+    REQUIRE(rows[1]["a"] == "4");
+    REQUIRE(rows[1]["b"] == "5");
+    REQUIRE(rows[1]["c"] == "6");
+  }
+}
