4dd83baed21c2290f79649e12b13878dc84a87d6
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-04-13 16:56:22 -0400

    Added second test for csv.filter

--- test/test_filter_rows.hpp
+++ test/test_filter_rows.hpp
@@ -15,3 +15,23 @@ TEST_CASE("Filter a simple csv", "[simple csv]") {
     REQUIRE(rows[0]["c"] == "6");
   }
 }
+
+TEST_CASE("Filter a simple csv II", "[simple csv]") {
+  csv::Reader csv;
+
+  csv.configure_dialect("students")
+    .skip_initial_space(true);
+
+  if (csv.read("inputs/test_15.csv")) {
+    auto rows = csv.filter([](auto row) {
+      return row["grade"] == "F";
+    });
+    REQUIRE(rows.size() == 2);
+    REQUIRE(rows[0]["first_name"] == "Tom");
+    REQUIRE(rows[0]["last_name"] == "Smith");
+    REQUIRE(rows[0]["grade"] == "F");
+    REQUIRE(rows[1]["first_name"] == "Jane");
+    REQUIRE(rows[1]["last_name"] == "Dee");
+    REQUIRE(rows[1]["grade"] == "F");
+  }
+}
