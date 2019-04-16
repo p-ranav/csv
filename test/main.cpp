ce79245ddf003a55b674354c52524b52da9089a7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-04-16 19:48:59 -0400

    Moved all tests into a single tests.hpp file since there aren't that many tests anyway

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,3 @@
 #define CATCH_CONFIG_MAIN
 #include <iostream>
-#include <test_basic_csv.hpp>
-#include <test_double_quotes.hpp>
-#include <test_ignore_columns.hpp>
+#include <tests.hpp>
--- test/tests.hpp
+++ test/tests.hpp
@@ -11,6 +11,18 @@ TEST_CASE("Parse an empty CSV", "[simple csv]") {
   REQUIRE(cols.size() == 0);
 }
 
+TEST_CASE("Parse file that doesn't exist", "[simple csv]") {
+  csv::Reader csv;
+  bool exception_thrown = false;
+  try {
+    csv.read("inputs/missing.csv");
+  }
+  catch (std::exception&) {
+    exception_thrown = true;
+  }
+  REQUIRE(exception_thrown);
+}
+
 TEST_CASE("Parse the most basic of CSV buffers", "[simple csv]") {
   csv::Reader csv;
   csv.read("inputs/test_01.csv");
@@ -240,4 +252,96 @@ TEST_CASE("Parse messed up log files", "[simple csv]") {
   REQUIRE(rows[1]["Thread ID"] == "02");
   REQUIRE(rows[1]["Log Level"] == "DEBUG");
   REQUIRE(rows[1]["Log Message"] == "Warning! Foo has happened");
+}
+
+TEST_CASE("Parse headers with double quotes", "[simple csv]") {
+  csv::Reader csv;
+  csv.read("inputs/test_06.csv");
+  auto rows = csv.rows();
+  REQUIRE(rows.size() == 0);
+  auto cols = csv.cols();
+  REQUIRE(cols.size() == 3);
+  REQUIRE(cols[0] == "\"Free trip to A,B\"");
+  REQUIRE(cols[1] == "\"5.89\"");
+  REQUIRE(cols[2] == "\"Special rate \"\"1.79\"\"\"");
+}
+
+TEST_CASE("Parse headers with pairs of single-quotes", "[simple csv]") {
+  csv::Reader csv;
+  csv.configure_dialect("test_dialect")
+    .quote_character('\'');
+  csv.read("inputs/test_07.csv");
+  auto rows = csv.rows();
+  REQUIRE(rows.size() == 0);
+  auto cols = csv.cols();
+  REQUIRE(cols.size() == 3);
+  REQUIRE(cols[0] == "''Free trip to A,B''");
+  REQUIRE(cols[1] == "''5.89''");
+  REQUIRE(cols[2] == "''Special rate ''''1.79''''''");
+}
+
+TEST_CASE("Parse the most basic of CSV buffers and ignore 1 column", "[simple csv]") {
+  csv::Reader csv;
+  csv.configure_dialect("test_dialect")
+    .ignore_columns("a");
+  csv.read("inputs/test_01.csv");
+  auto rows = csv.rows();
+  REQUIRE(rows.size() == 2);
+  REQUIRE(rows[0].count("a") == 0);
+  REQUIRE(rows[0]["b"] == "2");
+  REQUIRE(rows[0]["c"] == "3");
+  REQUIRE(rows[1].count("a") == 0);
+  REQUIRE(rows[1]["b"] == "5");
+  REQUIRE(rows[1]["c"] == "6");
+}
+
+TEST_CASE("Parse the most basic of CSV buffers and ignore 2 columns", "[simple csv]") {
+  csv::Reader csv;
+  csv.configure_dialect("test_dialect")
+    .ignore_columns("a", "b");
+  csv.read("inputs/test_01.csv");
+  auto rows = csv.rows();
+  REQUIRE(rows.size() == 2);
+  REQUIRE(rows[0].count("a") == 0);
+  REQUIRE(rows[0].count("b") == 0);
+  REQUIRE(rows[0]["c"] == "3");
+  REQUIRE(rows[1].count("a") == 0);
+  REQUIRE(rows[1].count("b") == 0);
+  REQUIRE(rows[1]["c"] == "6");
+}
+
+TEST_CASE("Parse the most basic of CSV buffers and ignore all columns", "[simple csv]") {
+  csv::Reader csv;
+  csv.configure_dialect("test_dialect")
+    .ignore_columns("a", "b", "c");
+  csv.read("inputs/test_01.csv");
+  auto rows = csv.rows();
+  REQUIRE(rows.size() == 2);
+  REQUIRE(rows[0].size() == 0);
+  REQUIRE(rows[1].size() == 0);
+}
+
+TEST_CASE("Parse the most basic of CSV buffers and ignore age/gender columns", "[simple csv]") {
+  csv::Reader csv;
+  csv.configure_dialect("test_dialect")
+    .delimiter(", ")
+    .ignore_columns("age", "gender");
+  csv.read("inputs/test_14.csv");
+  auto rows = csv.rows();
+  REQUIRE(rows.size() == 3);
+  REQUIRE(rows[0]["name"] == "Mark Johnson");
+  REQUIRE(rows[0]["email"] == "mark.johnson@gmail.com");
+  REQUIRE(rows[0]["department"] == "BA");
+  REQUIRE(rows[0].count("age") == 0);
+  REQUIRE(rows[0].count("gender") == 0);
+  REQUIRE(rows[1]["name"] == "John Stevenson");
+  REQUIRE(rows[1]["email"] == "john.stevenson@gmail.com");
+  REQUIRE(rows[1]["department"] == "IT");
+  REQUIRE(rows[1].count("age") == 0);
+  REQUIRE(rows[1].count("gender") == 0);
+  REQUIRE(rows[2]["name"] == "Jane Barkley");
+  REQUIRE(rows[2]["email"] == "jane.barkley@gmail.com");
+  REQUIRE(rows[2]["department"] == "MGT");
+  REQUIRE(rows[2].count("age") == 0);
+  REQUIRE(rows[2].count("gender") == 0);
 }
